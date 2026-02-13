a84fffa384165f7854349438d2c8a3d8b68eaa9d
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-02-13 22:55:52 +0100

    BUILD : example de code de modularité en RUST

---
+++ multiple_files_example/code.rs
@@ -0,0 +1,10 @@
+
+mod bib ;
+
+fn main(){
+
+   println!("The sum is {:?}", bib::addition(2,3));
+   println!("The subtraction is {:?}", bib::substract(2,3));
+    println!("The subtraction is {:?}", bib::multiply(2,3));
+   println!("The division  is {:?}", bib::divide(2,3));
+}
