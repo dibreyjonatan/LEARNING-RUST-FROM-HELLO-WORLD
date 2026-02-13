748204dc3b72dd762b9327bdd4b7877a28c86dd1
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-02-13 23:42:10 +0100

    BUILD : implementation du module math.rs

--- multiple_files_example/code.rs
+++ multiple_files_example/code.rs
@@ -1,10 +1,12 @@
 
 mod bib ;
-
+mod math ;
+use math::{factorial};
 fn main(){
 
    println!("The sum is {:?}", bib::addition(2,3));
    println!("The subtraction is {:?}", bib::substract(2,3));
     println!("The subtraction is {:?}", bib::multiply(2,3));
    println!("The division  is {:?}", bib::divide(2,3));
+   println!(" The factorial is {:?}", factorial(4)); 
 }
